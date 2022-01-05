61b5fa2d08ad34b61b2a4aed8a7d1515f6b03f25
leesoyuun <leesoyun4206@naver.com>
2022-01-05 16:44:31 +0900

    programmers - 68644

---
+++ Lv1/두개 뽑아서 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    var answer = [];
+    var sum =[];
+    for(let i = 0; i < numbers.length -1; i++){
+        for(let j = i+1 ; j<numbers.length; j++){
+            sum = numbers[i] + numbers[j]
+            if(!answer.includes(sum))
+                answer.push(sum)
+        }
+    }
+    return answer.sort((a,b)=>a-b);
+}
